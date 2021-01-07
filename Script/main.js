817d84f0b71acfc503dc8372e13896180d82d44c
Agustin Sarasua <64442857+a-sarasua@users.noreply.github.com>
2021-01-07 16:29:50 -0300

    Mejora botones

--- Script/main.js
+++ Script/main.js
@@ -3,16 +3,70 @@ const playButton = document.querySelector('.play')
 const nextButton = document.querySelector('.next')
 const prevButton = document.querySelector('.prev')
 const muteButton = document.querySelector('.mute')
-const maxButton = document.querySelector('.maxVolume')
 const volumeDownButton = document.querySelector('.volumeDown')
 const volumeUpButton = document.querySelector('.volumeUp')
+const information = document.querySelector('.information')
 
 const musicSource = '../Songs/'
+let musicVolume = 1.0
 
-playlist = ['1.mp3', '2.mp3', '3.mp3', '4.mp3', '5.mp3', '6.mp3', '7.mp3', '8.mp3']
+const playlist = [
+	{
+		file: '1.mp3',
+		songName: 'Sin Sobresaltos',
+		artist: 'Soda Stereo',
+	}, 
+	{
+		file: '2.mp3',
+		songName: 'El Rito',
+		artist: 'Soda Stereo',		
+	}, 
+	{
+		file: '3.mp3',
+		songName: 'Prófugos',
+		artist: 'Soda Stereo',
+	}, 
+	{
+		file: '4.mp3',
+		songName: 'No Existes',
+		artist: 'Soda Stereo',
+	}, 
+	{
+		file: '5.mp3',
+		songName: 'Persiana Americana',
+		artist: 'Soda Stereo',		
+	}, 
+	{
+		file: '6.mp3',
+		songName: 'En Camino',
+		artist: 'Soda Stereo',		
+	}, 
+	{
+		file: '7.mp3',
+		songName: 'Signos',
+		artist: 'Soda Stereo',		
+	}, 
+	{
+		file: '8.mp3',
+		songName: 'Final Caja Negra',
+		artist: 'Soda Stereo',		
+	},
+]
 
-audio.src = musicSource + playlist[0]
-playlistCounter = 0
+audio.src = musicSource + playlist[0].file
+let playlistCounter = 0
+
+function playPause() {
+	if (audio.paused) {
+		playButton.classList.remove('play')
+		playButton.classList.add('pause')			
+		audio.play()
+	} else {
+		playButton.classList.remove('pause')
+		playButton.classList.add('play')	
+		audio.pause()
+	}
+}
 
 function nextSong() {
 	if (playlistCounter === playlist.length-1) {
@@ -20,8 +74,8 @@ function nextSong() {
 	} else {
 		playlistCounter++
 	}
-	audio.src = musicSource + playlist[playlistCounter]
-	audio.play()
+	audio.src = musicSource + playlist[playlistCounter].file
+	playPause()
 }
 
 function prevSong() {
@@ -30,62 +84,57 @@ function prevSong() {
 	} else {
 		playlistCounter--
 	}	
-	audio.src = musicSource + playlist[playlistCounter]
-	audio.play()
+	audio.src = musicSource + playlist[playlistCounter].file
+	playPause()
 }
 
 function muteMusic() {
-	audio.volume = 0.0
-}
-
-function maxVolume() {
-	audio.volume = 1.0
+	if (audio.volume !== 0.0) {
+		audio.volume = 0.0
+		muteButton.classList.remove('mute')
+		muteButton.classList.add('maxVolume')	
+	} else {
+		muteButton.classList.remove('maxVolume')
+		muteButton.classList.add('mute')	
+		audio.volume = musicVolume
+	}	
 }
 
 function volumeDown() {
 	if (audio.volume !== 0.0) {
-		audio.volume -= 0.1
+		musicVolume -= 0.1
+		audio.volume = musicVolume
 	}
 }
 
 function volumeUp() {
-	if (audio.volume !== 1.0) {
-		audio.volume += 0.1
+	if (audio.volume === 0.0) {
+		musicVolume = 0.1
+		muteMusic()
+	} else if (audio.volume < 1.0) {
+		musicVolume += 0.1
+		audio.volume = musicVolume
 	}
 }
 
-playButton.onclick = function() {
-	if (audio.paused) {
-		audio.play()
-	} else {
-		audio.pause()
-	}
-}		
-
-nextButton.onclick = function() {
-	nextSong()
+function displayInfo() {
+	information.innerHTML = playlist[playlistCounter].songName + ' - ' + playlist[playlistCounter].artist 
 }
 
-prevButton.onclick = function() {
-	prevSong()
-}
+playButton.onclick = function() {playPause()}		
 
-audio.addEventListener('ended', function() {
-	nextSong()
-})
+nextButton.onclick = function() {nextSong()}
 
-muteButton.onclick = function() {
-	muteMusic()
-}
+prevButton.onclick = function() {prevSong()}
 
-maxButton.onclick = function() {
-	maxVolume()
-}
+audio.addEventListener('ended', function() {nextSong()})
 
-volumeDownButton.onclick = function() {
-	volumeDown()
-}
+muteButton.onclick = function() {muteMusic()}
 
-volumeUpButton.onclick = function() {
-	volumeUp()
-}
+volumeDownButton.onclick = function() {volumeDown()}
+
+volumeUpButton.onclick = function() {volumeUp()}
+
+audio.onplay = function() {displayInfo()}
+
+//console.log(Math.floor(Math.random() * (7 - 0)) + 0)
